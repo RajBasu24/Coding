82921187ca0c090da1d3a34441355aea539f2f67
Raj Basu <rajbasu2003@gmail.com>
2023-09-29 22:58:09 +0530

    Add 2 Java Programs

---
+++ Java Projects/Print_2_CLAS.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class CLAS
+{
+    public static void main(String args[])
+    {
+        System.out.println("First Argument: "+args[0]);
+        System.out.println("Second Argument: "+args[1]);
+    }
+}
---
+++ Java Projects/Print_All_CLAS.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class CLAS
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<args.length;i++)
+        {
+            System.out.println("Argument "+(i+1)+": "+args[i]);
+        }
+    }
+}
